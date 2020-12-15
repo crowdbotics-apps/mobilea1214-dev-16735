5a8e74f99000488521a11105cbca08666543a985
Crowdbotics <team@crowdbotics.com>
2020-12-15 17:59:39 +0000

    Update store

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,6 +3,7 @@ export const registration_post_restauth_registration_create = data => ({
   type: types.REGISTRATION_POST_RESTAUTH_REGISTRATION_CREATE,
   data
 })
+
 export const registration_post_restauth_registration_createSucceeded = (
   response,
   starter
@@ -11,6 +12,7 @@ export const registration_post_restauth_registration_createSucceeded = (
   response,
   starter
 })
+
 export const registration_post_restauth_registration_createFailed = (
   error,
   starter
